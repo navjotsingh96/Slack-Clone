b2de1a43cebb40487f6a46779c647255e2733ba3
navjotsingh96 <navjot.singh96@web.de>
2022-07-09 14:10:41 +0200

    added firestore to app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'slack-clone-94f23',
+    appId: '1:579998181745:web:6e1d5af7a2c72543d7aed0',
+    storageBucket: 'slack-clone-94f23.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCW2DIzu8Jk5Spdmc2ROwQKuZiCcfybc_E',
+    authDomain: 'slack-clone-94f23.firebaseapp.com',
+    messagingSenderId: '579998181745',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'slack-clone-94f23',
+    appId: '1:579998181745:web:6e1d5af7a2c72543d7aed0',
+    storageBucket: 'slack-clone-94f23.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCW2DIzu8Jk5Spdmc2ROwQKuZiCcfybc_E',
+    authDomain: 'slack-clone-94f23.firebaseapp.com',
+    messagingSenderId: '579998181745',
+  },
   production: false
 };
 
