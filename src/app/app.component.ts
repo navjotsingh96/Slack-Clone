052f6254058dca386e89489f3e1b7a93927e101b
atnbn <86778371+atnbn@users.noreply.github.com>
2022-07-14 17:01:35 +0200

    few changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddChannelsComponent } from './dialog-add-channels/dialog-add-channels.component';
 import { AuthenticationService } from './services/authentication.service';
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,13 +14,6 @@ export class AppComponent {
 
   constructor(firestore: AngularFirestore,
     public dialog: MatDialog,
-    public authService: AuthenticationService,
-    private router: Router) {}
+  ){}
   
-
-  logout(){
-    this.authService.logout().subscribe(() => {
-      this.router.navigate(['login']);
-    });
-  }
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Auth, authState } from '@angular/fire/auth';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { from } from 'rxjs';
-
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { from, switchMap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  currentUser$ = authState(this.auth)
+  currentUser$ = authState(this.auth)            // Here we get the current user
   constructor(private auth: Auth) { }
 
-  login(username : string, password:string){
+  login(username : string, password:string){              // Here we login a user
   return from(signInWithEmailAndPassword(this.auth, username, password));
   }
 
-  logout(){
+  signUp(name:string ,email: string, password: string){     // Here we create a new user
+    return from(createUserWithEmailAndPassword(this.auth, email , password))
+    .pipe(switchMap(({ user }) => updateProfile(user, {displayName: name})));
+    
+  }
+
+  logout(){                                           // Here we logout a user
     return from(this.auth.signOut());
   }
 
