faeedeba202b33791a50aeb237718a812f17e989
navjotsingh96 <navjot.singh96@web.de>
2022-07-16 13:50:13 +0200

    added msgs to channels

--- src/app/chat-room/chat-room.component.ts
+++ src/app/chat-room/chat-room.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { ChatService } from '../services/chat.service';
-import { Chat } from '../interface/chat'; 
+import { Chat, ChatToJSON } from '../interface/chat';
 import { idToken } from '@angular/fire/auth';
 
 @Component({
@@ -16,8 +16,10 @@ export class ChatRoomComponent implements OnInit {
 
   chatID: any;
   messageID: any;
- 
+  channelID;
+  message;
   chat$: Chat;
+  chatsTry = new ChatToJSON()
 
   constructor(private route: ActivatedRoute, public chatService: ChatService, private firestore: AngularFirestore) {
     this.chat$ = {
@@ -25,45 +27,62 @@ export class ChatRoomComponent implements OnInit {
       chatID: '',
       channelID: ''
     };
-   }
+  }
 
   ngOnInit(): void {
-    
     // Aktuelle chat ID holen
-    this.route.params.subscribe((params) => {
-      this.chatID = params['id'];
-      // console.log('ChatID: ', this.chatID)
+    this.route.paramMap.subscribe((paramMap) => {
+      this.chatID = paramMap.get('id');
 
-      // this.chatService.loadCurrentChat(params['id']);
+      this.chat$.chatID = this.chatID;
+      console.log('got id', this.chatID);
+      this.getMessages();
+    });
+  }
 
-      this.chat$.chatID = this.chatID
+  getMessages() {
+    this.firestore
+      .collection(this.chatID)
+      .valueChanges()
+      .subscribe((message) => {
+        console.log('Messages', message);
+        /*  this.showChannelMessages(message) */
+        this.allMessages.push(message);
+        if (message[0]['chatID'] === this.chatID) {
+         this.showChannelMessages(message)
+        }
+      })
+    this.allMessages = []
+    console.log('This. all', this.allMessages);
+
+  }
+
+  showChannelMessages(message) {
+    message.forEach(ID => {
+      if (ID.chatID === this.chatID) {
+        this.allMessages.push(ID);
+        console.log('fromfunction', ID);
+      }
     });
 
 
-      this.firestore
-        .collection('messages')
-        // .doc(this.messageID)
-        .valueChanges({idField: 'customIdName'})
-        .subscribe((chat: any) => {
-          
-          this.allMessages = chat
-          console.log('Message from Firestore: ', this.allMessages);
-        });
-    }  
-
-  
-  
+  }
 
   submit() {
     this.firestore
-    .collection('messages')
-    .add(this.chat$)
-    .then( (message: any) => {
-      // console.log('message', message.id)
-      this.messageID = message.id
-    });
+      .collection(this.chatID)
+      .add(this.chat$)
+      .then((message: any) => {
+        console.log('Suceesful', message);
+        // console.log('message', message.id)
+        this.messageID = message.id
+      }).catch((err) => {
+        console.log('Error', err);
 
+      })
     this.chat$.message = '';
-  }
 
+
+  }
 }
+
--- src/app/interface/chat.ts
+++ src/app/interface/chat.ts
@@ -3,3 +3,22 @@ export interface Chat {
     chatID: string,
     channelID: string
 }
+export class ChatToJSON {
+    message: string;
+    chatID: string;
+    channelID: string;
+
+
+    constructor(msg?: any) {
+        this.message = msg ? msg.message : '';
+        this.chatID = msg ? msg.chatID : '';
+        this.channelID = msg ? msg.channelID : '';
+    }
+    public toJSON() {
+        return {
+            message: this.message,
+            chatID: this.chatID,
+            channelID: this.channelID,
+        }
+    }
+}
