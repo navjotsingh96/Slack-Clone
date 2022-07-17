0655f4ca6f34467d33112c84bbfe3c0814676612
atnbn <86778371+atnbn@users.noreply.github.com>
2022-07-17 14:05:45 +0200

    new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { ChatRoomComponent } from './chat-room/chat-room.component';
 import { HomeComponent } from './home/home.component';
 import { ThreadComponent } from './thread/thread.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { LoginComponent } from './login/login/login.component';
-import { SignUpComponent } from './login/sign-up/sign-up.component';
+import { LoginComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 import {canActivate, redirectUnauthorizedTo, redirectLoggedInTo} from '@angular/fire/auth-guard'; 
 
 const redirectToLogin = () => redirectUnauthorizedTo(['login']);
@@ -14,8 +14,8 @@ const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'chat', component: ChatRoomComponent
     , canActivate: [canActivate(redirectToLogin)] },
-  { path: 'theard', component: ThreadComponent },
-  { path: 'sidebar', component: SidebarComponent },
+  { path: 'theard', component: ThreadComponent,...canActivate(redirectTooHome) },
+  { path: 'sidebar', component: SidebarComponent,...canActivate(redirectTooHome) },
 
   {
     path: 'login',
@@ -23,9 +23,9 @@ const routes: Routes = [
     ...canActivate(redirectTooHome) // redirects to home if logged in
   },
   {
-    path: 'login/sign-up',
+    path: 'sign-up',
     component: SignUpComponent,
-    ...canActivate(redirectTooHome)   // redirectToLogin if 
+    ...canActivate(redirectToLogin)   // redirectToLogin if 
   },
 
   {
