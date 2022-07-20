6cba6baacdaef9c21fbd1620f804c12c683fc00a
atnbn <86778371+atnbn@users.noreply.github.com>
2022-07-20 11:32:40 +0200

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-    ...canActivate(redirectToLogin) // redirects to home if logged in
+    ...canActivate(redirectTooHome) // redirects to home if logged in
   },
   {
     path: 'sign-up',
